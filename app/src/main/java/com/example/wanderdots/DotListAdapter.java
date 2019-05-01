09ce785f822a2017e07a63299d31d03a5c188222
ryank262 <36464202+ryank262@users.noreply.github.com>
2019-05-01 14:05:19 -0700

    Added a List of Dots to the Home Page

---
+++ app/src/main/java/com/example/wanderdots/DotListAdapter.java
@@ -0,0 +1,71 @@
+package com.example.wanderdots;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class DotListAdapter extends RecyclerView.Adapter<DotListAdapter.ViewHolder> {
+
+    public ArrayList<Dot> dotList;
+    Context ctx;
+
+    public DotListAdapter(ArrayList<Dot> dotList, Context ctx){
+        this.dotList = dotList;
+    }
+
+    @NonNull
+    @Override
+    public DotListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.dot_list_item, viewGroup, false);
+        this.ctx = ctx;
+        return new ViewHolder(view, ctx, dotList);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DotListAdapter.ViewHolder viewHolder, int i) {
+        viewHolder.title.setText(dotList.get(i).getTitle());
+        viewHolder.distance.setText(new Double(dotList.get(i).getLatitude()).toString());
+        viewHolder.rating.setText("5 Stars");
+    }
+
+    @Override
+    public int getItemCount() {
+        return dotList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        View mView;
+
+        public TextView title;
+        public TextView distance;
+        public TextView rating;
+        ArrayList<Dot> dotList = new ArrayList<Dot>();
+        Context ctx;
+
+        public ViewHolder(@NonNull View itemView, Context ctx, ArrayList<Dot> dots){
+            super(itemView);
+            mView = itemView;
+            this.ctx = ctx;
+            this.dotList = dots;
+            itemView.setOnClickListener(this);
+
+            title = (TextView) mView.findViewById(R.id.dot_item_title);
+            distance = (TextView) mView.findViewById(R.id.dot_item_dist);
+            rating = (TextView) mView.findViewById(R.id.dot_item_rating);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(this.ctx, DotDetailActivity.class);
+            this.ctx.startActivity(intent);
+        }
+    }
+}
--- app/src/main/java/com/example/wanderdots/MainActivity.java
+++ app/src/main/java/com/example/wanderdots/MainActivity.java
@@ -4,21 +4,45 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private FloatingActionButton newDotBtn;
-    private Button dotDetailBtn;
+    private RecyclerView main_list;
+
+    private ArrayList<Dot> dotList;
+    private DotListAdapter dotListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /* Connect GUI elements with code here */
         newDotBtn = (FloatingActionButton) findViewById(R.id.new_dot_btn);
-        dotDetailBtn = (Button) findViewById(R.id.dot_detail_btn);
+        main_list = (RecyclerView) findViewById(R.id.main_recycler_view);
+        dotList = new ArrayList<Dot>();
+        dotListAdapter = new DotListAdapter(dotList, this);
+
+        main_list.setHasFixedSize(true);
+        main_list.setLayoutManager(new LinearLayoutManager(this));
+        main_list.setAdapter(dotListAdapter);
+
+
+        dotList.add(new Dot(32, "Avila Caves", "Cool Caves", 33.1, 127.6, 36));
+        dotList.add(new Dot(32, "Bishops Peak", "Cool Caves", 3.2, 127.6, 36));
+        dotList.add(new Dot(32, "Terrace Hill", "Cool Caves", 66.4, 127.6, 36));
+        dotList.add(new Dot(32, "Morro Bay", "Cool Caves", 44.2, 127.6, 36));
+        dotList.add(new Dot(32, "Cayucos", "Cool Caves", 1.0, 127.6, 36));
+        dotList.add(new Dot(32, "Cuesta Ridge", "Cool Caves", 0.5, 127.6, 36));
+        dotListAdapter.notifyDataSetChanged();
+
 
         newDotBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,13 +52,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        dotDetailBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, DotDetailActivity.class);
-                startActivity(intent);
-            }
-        });
 
         /* TODO: this needs to be wrapped in an if to see if the user is
             is already logged in */
