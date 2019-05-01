700e6da3cdd8f576c70fa042e3d97fe453f7f242
aegarro <agarro@calpoly.edu>
2019-05-01 15:22:43 -0700

    dotDetail shows title of each dot

--- app/src/main/java/com/example/wanderdots/DotDetailActivity.java
+++ app/src/main/java/com/example/wanderdots/DotDetailActivity.java
@@ -1,13 +1,22 @@
 package com.example.wanderdots;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DotDetailActivity extends AppCompatActivity {
+
+    private TextView titleTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dot_detail);
+
+        titleTxt = (TextView) findViewById(R.id.dotTitle);
+        Intent intent = getIntent();
+        titleTxt.setText(intent.getStringExtra("title"));
     }
 }
