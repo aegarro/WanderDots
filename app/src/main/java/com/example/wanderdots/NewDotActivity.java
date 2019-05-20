8decd13aa6adf7885239b1a0071d1027e04d85b6
azilvitis <azilviti@calpoly.edu>
2019-05-20 11:35:49 -0700

    most addDot functionality complete, TODO: multi-selection for categories, multiple images, correct image upload, location picker

--- app/src/main/java/com/example/wanderdots/NewDotActivity.java
+++ app/src/main/java/com/example/wanderdots/NewDotActivity.java
@@ -1,5 +1,14 @@
 package com.example.wanderdots;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +16,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.content.Intent;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 import WanderDots.Dot ;
 import WanderDots.Server.Post.PostDot;
 import WanderDots.Observer ;
@@ -17,22 +34,68 @@ public class NewDotActivity extends AppCompatActivity
     private Button createButton ;
     private EditText nameTextbox ;
     private EditText descriptionTextbox ;
-    private EditText categoriesTextbox ;
+    private TextView categoriesView ;
+    private ImageView imageView4;
+    private ImageButton imageButton ;
     private String dotID ;
     private PostDot dotCreator ;
+    private static int RESULT_LOAD_IMAGE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_dot);
 
-        this.createButton = (Button) findViewById(R.id.CreateButton) ;
-        this.nameTextbox = (EditText) findViewById(R.id.NameTextbox) ;
-        this.descriptionTextbox = (EditText) findViewById(R.id.DescriptionTextbox) ;
+        this.createButton = findViewById(R.id.CreateButton) ;
+        this.nameTextbox = findViewById(R.id.NameTextbox) ;
+        this.descriptionTextbox = findViewById(R.id.DescriptionTextbox) ;
+        this.categoriesView = findViewById(R.id.CategoriesView);
+        this.imageButton = findViewById(R.id.imageButton);
+        this.imageView4 = findViewById(R.id.imageView4);
 
+        View.OnClickListener addImageListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addImages();
+            }
+        };
+        this.imageButton.setOnClickListener(addImageListener);
         this.createButton.setOnClickListener(this);
         this.dotCreator = new PostDot(super.getApplicationContext(), this);
         this.dotID = null ;
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 101);
+        }
+    }
+
+    private void addImages(){
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(photoPickerIntent, RESULT_LOAD_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        if (reqCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            try {
+                final Uri imageUri = data.getData();
+                if(imageUri != null) {
+                    final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                    final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                    imageView4.setImageBitmap(selectedImage);
+                    if (imageStream != null) {
+                        imageStream.close();
+                    }
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 
     public void dataHasChanged(String message){
@@ -51,15 +114,30 @@ public class NewDotActivity extends AppCompatActivity
     public void onClick(View v){
 
         Log.d("WanderDots", "About to create a Dot...") ;
-        //DUMMY CREATE
-        Dot dot = new Dot() ;
-        dot.setName("Dot created in studio");
-        dot.setCreator("RootUser");
-        dot.setDescription("Test dot for Post route from studio");
-        dot.addCategory("TestSet");
-        dot.addPictureId("testPictureID") ;
-        dot.setLatitude(35.123123123);
-        dot.setLongitude(-120.45);
+        //create
+        Dot dot = new Dot();
+        final EditText name = findViewById(R.id.NameTextbox);
+        dot.setName(name.getText().toString());
+        //Creator???
+        dot.setCreator("Root");
+        final EditText description = findViewById(R.id.DescriptionTextbox);
+        dot.setDescription(description.getText().toString());
+        //how to get data from multi-select (shaheen)
+        dot.addCategory("Filler");
+        //todo: multiple images
+        //what is the picture id? bitmap? file path?
+        //dot.addPictureId(selectedImage);
+        dot.addPictureId("testPictureID");
+        //how to make a map marker and get lat/long from that - for now stub using current loc
+        try {
+            LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            dot.setLongitude(location.getLongitude());
+            dot.setLatitude(location.getLatitude());
+        }
+        catch(SecurityException e){
+            e.printStackTrace();
+        }
 
         this.dotCreator.postDot(dot) ;
     }
