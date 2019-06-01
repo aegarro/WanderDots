fb9121e2f6e2a3e9535d182cf49616d59da04e2c
aegarro <agarro@calpoly.edu>
2019-06-01 15:39:39 -0700

    adding profile screens - not connected to main screen. Will not pop up on emulator

---
+++ app/src/main/java/com/example/wanderdots/UserProfileActivity.java
@@ -0,0 +1,33 @@
+package com.example.wanderdots;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        TextView titleTxt;
+        TextView distanceTxt;
+        TextView ratingTxt;
+        TextView describeTxt;
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dot_detail);
+
+
+        titleTxt = findViewById(R.id.dotTitle);
+        distanceTxt = findViewById(R.id.dotDistance);
+        ratingTxt = findViewById(R.id.dotRating);
+        describeTxt = findViewById(R.id.userDescription);
+
+        Intent intent = getIntent();
+        titleTxt.setText(intent.getStringExtra("title"));
+        distanceTxt.setText("Distance: " + intent.getStringExtra("distance"));
+        ratingTxt.setText("Rating: " + intent.getStringExtra("rating"));
+        describeTxt.setText("Description: ");
+
+    }
+}
