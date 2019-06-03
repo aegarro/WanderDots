5c97948e74ffa4ce3c3f93c68746fe373a28b565
albertorodriguez <arodr243@calpoly.edu>
2019-06-03 12:50:15 -0700

    Login screen now active. TODO: fix why activity is cancelled when logging in.

--- app/src/main/java/com/example/wanderdots/MainActivity.java
+++ app/src/main/java/com/example/wanderdots/MainActivity.java
@@ -4,21 +4,34 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.example.wanderdots.find.FindExperiencesActivity;
+
+import wanderdots.Adventure;
+import wanderdots.Dot;
 
 public class MainActivity extends AppCompatActivity {
 
     public static Context defaultContext;
+    private final int LOGIN_ID = 420 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.defaultContext = getApplicationContext() ;
-        Intent intent = new Intent(this, FindExperiencesActivity.class);
-        startActivity(intent);
+        Intent loginIntent = new Intent(this, LoginActivity.class) ;
+        startActivityForResult(loginIntent, LOGIN_ID);
     }
 
+    @Override
+    //Runs after CreateDotActivity
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == LOGIN_ID && resultCode == RESULT_CANCELED){
+            Intent intent = new Intent(this, FindExperiencesActivity.class);
+            startActivity(intent);
+        }
+    }
     //TODO: implement check for login token, and redirect if token not given
 
     //SHOULD BE USED FOR TESTING ONLY
